f19635c96a49f70dd77012453e9ebcf750abc32a
Brandon Harris <brandon@relentnet.com>
2024-12-31 00:34:46 -0600

    Updated the navbar links to show active if you're on a subpage of the original.

--- src/components/navbar/NavLinkItem.js
+++ src/components/navbar/NavLinkItem.js
@@ -1,17 +1,18 @@
 import { motion } from "framer-motion";
 
 export default function NavLinkItem({ href, currentPath, children, menuOpen }) {
+    const isActive =
+        href === "/" ? currentPath === href : currentPath.startsWith(href);
+
     return (
         <li>
             <motion.a
                 whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.95 }}
                 href={href}
-                aria-label={children + " link"}
-                className={`block font-bold px-4 py-2 md:p-0 active:text-accent text-xl ${menuOpen ? "text-center" : ""} ${currentPath === href
-                    ? "text-accent hover:text-accent"
-                    : "hover:text-subtext0"
-                }`}
+                aria-label={`${children} link`}
+                className={`block font-bold px-4 py-2 md:p-0 active:text-accent text-xl ${menuOpen ? "text-center" : ""
+                    } ${isActive ? "text-accent hover:text-accent" : "hover:text-subtext0"}`}
             >
                 {children}
             </motion.a>
