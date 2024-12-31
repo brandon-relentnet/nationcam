21ce3681bc2fd8cf7e3526a1039b2d5441f6f559
Brandon Harris <brandon@relentnet.com>
2024-12-31 00:12:13 -0600

    Fixed the scroll progress bar to better adapt to dynamically generated pages.

--- src/components/navbar/ScrollProgressBar.js
+++ src/components/navbar/ScrollProgressBar.js
@@ -1,13 +1,29 @@
 'use client';
 
 import { motion, useScroll } from 'framer-motion';
+import { useState, useEffect } from 'react';
 
 export default function ScrollProgressBar() {
     const { scrollYProgress } = useScroll();
+    const [isScrolling, setIsScrolling] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolling(true);
+        };
+
+        // Listen for the scroll event
+        window.addEventListener('scroll', handleScroll, { once: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <motion.div
-            className="absolute top-20 left-0 right-0 h-1 bg-accent transform-origin-[0%]"
+            className={`fixed top-20 left-0 right-0 h-1 bg-accent transform-origin-[0%] transition-opacity duration-300 ${isScrolling ? 'opacity-100' : 'opacity-0'
+                }`}
             style={{ scaleX: scrollYProgress }}
         />
     );
