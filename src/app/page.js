56b50f384eb87936cd225241b4b3a52af8d61f49
Brandon Harris <brandon@relentnet.com>
2025-01-02 14:49:43 -0600

    Started filling out the homepage, created a roadmap section and a FAQ section.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
+import FAQSection from "./FAQSection";
+import HomeHeroSection from "./HomeHeroSection";
+import RoadMap from "./RoadMap";
+
 export default function Home() {
   return (
-    <div className="page-container">
-      <h1>Home</h1>
-      <p>
-        Welcome to NationCam, a website dedicated to showing some of the best views in the country while giving you a taste for the local culture and life. Our network of cameras is slowly coming online so be sure to come back to see more.
-      </p>
-    </div>
+    <>
+      <HomeHeroSection />
+      <div className="page-container">
+        <RoadMap />
+        <FAQSection />
+      </div>
+    </>
   );
 }
