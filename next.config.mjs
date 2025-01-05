f5a8aae8bb61f01ea7b37148db150128a430d6c4
Brandon Harris <brandon@relentnet.com>
2025-01-04 19:41:30 -0600

    Added a redirect from /venicemarina -> /locations/sublocations/venice-marina.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+
+    async redirects() {
+        return [
+            {
+                source: '/venicemarina',
+                destination: '/locations/sublocations/venice-marina',
+                permanent: true, // Use true for a 301 redirect
+            },
+        ];
+    },
 };
 
 export default nextConfig;
